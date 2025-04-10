78ac703b80b6503079397692996087ac7d4074ae
Fibonacci <volokitin.vitaliy@icloud.com>
2025-04-10 12:42:01 +0300

    Added admin configurations for Position, Worker, TaskType, and Task models. - Extended the form for Worker, added 'position' field. - Set filters and search fields for Task. - Added display settings for Position and TaskType models.

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from board.models import Task, Position, Worker, TaskType
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position", )
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional information", {
+            "fields": ("position", )
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets +(
+        ("Additional Information", {
+            "fields": ("first_name", "last_name","position", )
+        }),
+    )
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "priority", "is_completed")
+    list_filter = ("priority", "is_completed")
+    search_fields = ("name", "description")
