e6a6e7fb10d53177448806915846c729c1fc64b3
Fibonacci <volokitin.vitaliy@icloud.com>
2025-04-25 14:17:41 +0300

    Add CRUD routes for tasks and detail view for workers
    - Added routes for TaskCreateView, TaskDetailView, TaskUpdateView, and TaskDeleteView
    - Added route for WorkerProfileDetailView

--- board/urls.py
+++ board/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 
-from board.views import index, TasksListView, WorkersListView
+from board.views import (
+    index, TasksListView, WorkersListView,
+    TaskCreateView, TaskDetailView, TaskUpdateView,
+    TaskDeleteView, WorkerProfileDetailView
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/", TasksListView.as_view(), name="task-list"),
-    path("workers/", WorkersListView.as_view(), name="worker-list")
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/create", TaskCreateView.as_view(), name="task-create"),
+    path("workers/", WorkersListView.as_view(), name="worker-list"),
+    path("workers/<int:pk>", WorkerProfileDetailView.as_view(), name="worker-profile-detail"),
 ]
 
 app_name = "board"
