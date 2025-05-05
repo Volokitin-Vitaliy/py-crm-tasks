da633d580dd1752c19852abd29f04d9f46ffb77e
Fibonacci <volokitin.vitaliy@icloud.com>
2025-05-05 11:24:58 +0300

    Add routes for creating, updating, and deleting worker profiles

--- board/urls.py
+++ board/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from board.views import (
     index, TasksListView, WorkersListView,
     TaskCreateView, TaskDetailView, TaskUpdateView,
-    TaskDeleteView, WorkerProfileDetailView
+    TaskDeleteView, WorkerProfileDetailView, WorkerCreateView,
+    WorkerUpdateView, WorkerDeleteView,
 )
 
 urlpatterns = [
@@ -14,6 +15,9 @@ urlpatterns = [
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tasks/create", TaskCreateView.as_view(), name="task-create"),
     path("workers/", WorkersListView.as_view(), name="worker-list"),
+    path("workers/create/", WorkerCreateView.as_view(), name="worker-create"),
+    path("workers/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
+    path("workers/<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete"),
     path("workers/<int:pk>", WorkerProfileDetailView.as_view(), name="worker-profile-detail"),
 ]
 
